f0e20cc6c0c8ab9eaf94db68c7b6e47e4570b4ce
Waren Gonzaga <opensource@warengonzaga.com>
2025-08-07 14:20:01 +0000

    ✨ tweak: add permission checks

--- src/commands/support/support.js
+++ src/commands/support/support.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, ModalBuilder, ActionRowBuilder, TextInputBuilder, TextInputStyle } = require('discord.js');
+const { SlashCommandBuilder, ModalBuilder, ActionRowBuilder, TextInputBuilder, TextInputStyle, PermissionFlagsBits } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -9,6 +9,24 @@ module.exports = {
         // Check if the command is used in a thread and if so, prevent execution
         if (interaction.channel.isThread && interaction.channel.isThread()) {
             await interaction.reply({ content: 'The `/support` command can only be used in a text channel.', ephemeral: true });
+            return;
+        }
+
+        // Check if bot has necessary permissions to create threads in this channel
+        const botMember = interaction.guild.members.me;
+        const requiredPermissions = [
+            PermissionFlagsBits.ManageThreads,
+            PermissionFlagsBits.CreatePrivateThreads,
+            PermissionFlagsBits.SendMessages,
+            PermissionFlagsBits.SendMessagesInThreads,
+            PermissionFlagsBits.ViewChannel
+        ];
+
+        if (!botMember.permissionsIn(interaction.channel).has(requiredPermissions)) {
+            await interaction.reply({
+                content: 'I don\'t have the necessary permissions to create support tickets in this channel. Please use this command in an authorized channel or contact an administrator.',
+                ephemeral: true
+            });
             return;
         }
 
